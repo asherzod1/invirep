f240b49843f6523170241abf559339917adeb1ce
sherzod_a_thinkland <sherzod_a@thinkland.uz>
2023-08-20 17:34:25 +0500

    invirep google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 // import { DefaultSeo } from 'next-seo';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+        <meta name="google-site-verification" content="EeNrX_Vhd4bMEJnslGv6D89shImpFKT1eTXHeeqUF8I" />
+    </head>
       <body className={inter.className}>
       {/*<DefaultSeo*/}
       {/*    title="Invirep"*/}
