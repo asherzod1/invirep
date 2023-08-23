c773399b282196904a2e334efe728f37ec5a8569
sherzod_a_thinkland <sherzod_a@thinkland.uz>
2023-08-23 12:52:35 +0500

    fix seo

--- src/app/page.js
+++ src/app/page.js
@@ -5,40 +5,7 @@ export default function Home() {
 
     return (
         <>
-            {/*<NextSeo*/}
-            {/*    title="Invirep - ИНВИРЕП"*/}
-            {/*    description="Invirep - ИНВИРЕП"*/}
-            {/*    canonical="https://www.invirep.uz/"*/}
-            {/*    openGraph={{*/}
-            {/*        url: 'https://invirep.uz/',*/}
-            {/*        title: 'Invirep - ИНВИРЕП',*/}
-            {/*        description: 'Invirep - ИНВИРЕП',*/}
-            {/*        images: [*/}
-            {/*            {*/}
-            {/*                url: 'https://www.example.ie/og-image-01.jpg',*/}
-            {/*                width: 800,*/}
-            {/*                height: 600,*/}
-            {/*                alt: 'Og Image Alt',*/}
-            {/*                type: 'image/jpeg',*/}
-            {/*            },*/}
-            {/*            {*/}
-            {/*                url: 'https://www.example.ie/og-image-02.jpg',*/}
-            {/*                width: 900,*/}
-            {/*                height: 800,*/}
-            {/*                alt: 'Og Image Alt Second',*/}
-            {/*                type: 'image/jpeg',*/}
-            {/*            },*/}
-            {/*            { url: 'https://www.example.ie/og-image-03.jpg' },*/}
-            {/*            { url: 'https://www.example.ie/og-image-04.jpg' },*/}
-            {/*        ],*/}
-            {/*        siteName: 'SiteName',*/}
-            {/*    }}*/}
-            {/*    twitter={{*/}
-            {/*        handle: '@handle',*/}
-            {/*        site: '@site',*/}
-            {/*        cardType: 'summary_large_image',*/}
-            {/*    }}*/}
-            {/*/>*/}
+
             <div className="xl:max-w-[1110px] my-0 mx-auto w-[90%]">
                 <div className="h-[80px] flex items-center justify-between">
                     <a href="#"><img
